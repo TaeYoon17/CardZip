f53f15190d2ec79daf2327f6e2e639c238718544
TaeYoon17 <kim0517@hotmail.com>
2023-10-18 20:39:44 +0900

    New) 집중 암기 항목, 암기 대상 항목 선택 기능 추가 Update) 카드가 없을 때 나타나는 Scene 오류들 수정, Set 글자 수 관련 깨짐 현상 오류 수정

---
+++ CardZip/Helper/Extensions/UIs/ViewControllerExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  ViewControllerExtensions.swift
+//  CardZip
+//
+//  Created by 김태윤 on 2023/10/16.
+//
+
+import Foundation
---
+++ CardZip/SceneCores/SceneComponents/UIBase/EditableVC.swift
@@ -0,0 +1,8 @@
+//
+//  EditableVC.swift
+//  CardZip
+//
+//  Created by 김태윤 on 2023/10/16.
+//
+
+import Foundation
