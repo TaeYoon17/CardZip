a3a58ab28e8c4a1ab62eeb703871968b1edfb4b7
TaeYoon17 <kim0517@hotmail.com>
2023-10-20 03:17:48 +0900

    New) 다국어 및 음성 지원 대응

---
+++ CardZip/Services/Localized/Localize.swift
@@ -0,0 +1,8 @@
+//
+//  Localize.swift
+//  CardZip
+//
+//  Created by 김태윤 on 2023/10/20.
+//
+
+import Foundation
---
+++ CardZip/Services/TTS_Service.swift
@@ -0,0 +1,8 @@
+//
+//  TTS_Service.swift
+//  CardZip
+//
+//  Created by 김태윤 on 2023/10/19.
+//
+
+import Foundation
