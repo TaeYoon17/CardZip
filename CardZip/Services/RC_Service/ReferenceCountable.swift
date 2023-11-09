a0d9bfe77160f45f8a0a4b2708f10bedcf3b514e
TaeYoon17 <kim0517@hotmail.com>
2023-11-09 22:37:22 +0900

    Update v.1.1) Image Reference Counter를 File Reference Counter로 확장

---
+++ CardZip/Services/RC_Service/ReferenceCountable.swift
@@ -0,0 +1,8 @@
+//
+//  ReferenceCountable.swift
+//  CardZip
+//
+//  Created by 김태윤 on 2023/11/09.
+//
+
+import Foundation
---
+++ CardZip/Services/RC_Service/ReferenceCounter.swift
@@ -0,0 +1,8 @@
+//
+//  ReferenceCounter.swift
+//  CardZip
+//
+//  Created by 김태윤 on 2023/11/09.
+//
+
+import Foundation
